7323bb6a70436d220ce8c6c2dd38cb9b5893c878
VinceS42 <v.fayon.pro@gmail.com>
2024-03-26 12:45:37 +0100

    init supabase et prisma

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import { hostname } from "os";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            { hostname: "a0.muscache.com", protocol: "https", port: "" },
+        ],
+    },
+};
 
 export default nextConfig;
